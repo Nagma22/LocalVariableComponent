c9135596e65ebe5e07f00d9ff9be360e252b15ce
Nagma22 <nagmashima7@gmail.com>
2018-12-13 23:11:05 +0000

    LocalVariable commiting

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent {
+
+  count= 0;
+
+  increment()
+  {
+    this.count++;
+  }
+
+  decrement()
+  {
+    this.count--;
+  }
+
+}
